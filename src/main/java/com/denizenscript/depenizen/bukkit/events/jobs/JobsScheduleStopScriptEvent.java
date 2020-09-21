4a3ce8ce7a5400c7c0cbc0202005ea9c4839bfdf
pjochillin <pjochillin@gmail.com>
2020-09-20 20:12:44 -0400

    Update Fix for Other Related Event

--- src/main/java/com/denizenscript/depenizen/bukkit/events/jobs/JobsScheduleStopScriptEvent.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/events/jobs/JobsScheduleStopScriptEvent.java
@@ -63,10 +63,11 @@ public class JobsScheduleStopScriptEvent extends BukkitScriptEvent implements Li
 
     @Override
     public boolean applyDetermination(ScriptPath path, ObjectTag determinationObj) {
-        if (Argument.valueOf(determinationObj.toString()).matchesArgumentList(JobsJobTag.class)) {
+        String determination = determinationObj.toString();
+        if (Argument.valueOf(determination).matchesArgumentList(JobsJobTag.class)) {
             List<String> list = new ArrayList<>();
-            for (int i = 0; i < ((ListTag) determinationObj).size(); i++) {
-                list.add(((JobsJobTag) ((ListTag) determinationObj).getObject(i)).getJob().getName());
+            for (JobsJobTag j : ListTag.valueOf(determination, getTagContext(path)).filter(JobsJobTag.class, path.container, true)) {
+                list.add(j.getJob().getName());
             }
             event.getSchedule().setJobs(list);
             return true;
